8539e5b2121078be508a638a35799309d60ced12
Razan majdy <razanwork2005@gmail.com>
2025-04-14 05:13:23 +0300

    Update Main.java

--- multi-lang-demo/Java/Main.java
+++ multi-lang-demo/Java/Main.java
@@ -1,5 +1,31 @@
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessTheNumber {
     public static void main(String[] args) {
-        System.out.println("Hello from Java!");
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+
+        int secretNumber = rand.nextInt(100) + 1; // رقم بين 1 و 100
+        int guess;
+        int attempts = 0;
+
+        System.out.println("مرحباً! حاول تخمين الرقم بين 1 و 100");
+
+        do {
+            System.out.print("ادخل رقمك: ");
+            guess = input.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("أعلى!");
+            } else if (guess > secretNumber) {
+                System.out.println("أقل!");
+            } else {
+                System.out.println("🎉 مبروك! خمنت الرقم بعد " + attempts + " محاولة.");
+            }
+        } while (guess != secretNumber);
+
+        input.close();
     }
 }
